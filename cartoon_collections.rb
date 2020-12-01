914d5e08b671124a7872a51959ddee94891d392c
Esma Sherwani <esmasherr@gmail.com>
2020-12-01 21:04:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,4 +25,5 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   long_planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  expect(long_planteer_calls(long_planeteer_calls)).to eq(true)
 end
